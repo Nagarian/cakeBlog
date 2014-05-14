9aac9be13c36cf2c08e4743ccdebba96e49b2ea4
Florian Hollécou <failron@hotmail.fr>
2014-05-14 18:48:25 +0200

    Create PartenairesController.php
    Création du controller pour la page partenaires.

---
+++ Controller/PartenairesController.php
@@ -0,0 +1,12 @@
+<?php
+App::uses('AppController', 'Controller');
+
+class PartenairesController extends AppController {
+
+    public function index() {
+    }
+
+    public function view($id = null) {
+    }
+
+}
